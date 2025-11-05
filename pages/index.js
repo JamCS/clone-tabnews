d659efbde3058262fb6503a4aadb5b32ccbdefd3
JamCS <jamilsoncnn777@gmail.com>
2025-11-05 22:56:24 +0000

    modifica o arquivo index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function Home() {
-  return <h1>Estou estudando e ja ja casamos amor, te amo</h1>;
+  return (
+    <h1>
+      "A vida é dolorosa demais para ser suportada em sua forma pura, por isso
+      nós a distorcemos — criamos ilusões, sonhos e histórias para torná-la
+      suportável."- Charlie Mugen
+    </h1>
+  );
 }
 
 export default Home;
