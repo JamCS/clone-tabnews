a168dc2a086a79834517ff36c07142d04a4f6887
JamCS <jamilsoncnn777@gmail.com>
2026-06-02 18:10:22 +0000

    adds `/api/v1/migrations` endpoint

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+// const { default: next } = require("next");
+const dotenv = require("dotenv");
+dotenv.config({
+  path: ".env.development",
+});
+
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  dir: ".",
+});
+const jestConfig = createJestConfig({
+  moduleDirectories: ["node_modules", "<rootDir>"],
+});
+
+module.exports = jestConfig;
