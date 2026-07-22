424a5fdcc98b7d591e43f57f8f5c7c053144dd97
JamCS <jamilsoncnn777@gmail.com>
2026-07-22 05:25:13 +0000

    fix: fix sementic error in `pages/index.js`

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,19 @@
 function Home() {
   return (
-    <p>
-      <h2>
-        ¹² Portanto, como povo escolhido de Deus, santo e amado, revistam-se de
-        profunda compaixão, bondade, humildade, mansidão e paciência. <br />
-        ¹³ Suportem-se uns aos outros e perdoem as queixas que tiverem uns
-        contra os outros. Perdoem como o Senhor lhes perdoou.
-        <br />
-        ¹⁴ Acima de tudo, porém, revistam-se do amor, que é o elo perfeito.
-        <br />
-      </h2>
-      <h1>Colossenses 3:12-14</h1>
-    </p>
+    <div>
+      <section>
+        <h2>
+          ¹² Portanto, como povo escolhido de Deus, santo e amado, revistam-se
+          <br />
+          ¹³ Suportem-se uns aos outros e perdoem as queixas que tiverem uns
+          contra os outros. Perdoem como o Senhor lhes perdoou.
+          <br />
+          ¹⁴ Acima de tudo, porém, revistam-se do amor, que é o elo perfeito.
+          <br />
+        </h2>
+        <h1>Colossenses 3:12-14</h1>
+      </section>
+    </div>
   );
 }
 
